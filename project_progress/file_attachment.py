9f1b552eabda070c22dbbfcebc34dee91acdf889
KeunWoo <104286190+kw9212@users.noreply.github.com>
2023-03-28 02:19:55 -0400

    Create file_attachment.py

---
+++ project_progress/file_attachment.py
@@ -0,0 +1,43 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+# Enter sender's Google account and PW
+send_email = "Google account@gmial.com"
+send_pwd = "Google PW"
+
+# Enter receiver's Google address
+recv_email = "Email address"
+
+smtp_name = "smtp.gmail.com"
+smtp_port = 587
+
+msg = MIMEMultipart()
+
+msg['Subject'] = "This is a file attachment test"
+msg['From'] = send_email
+msg['To'] = recv_email
+
+text = """
+This is the sample email contect.
+"""
+
+contentPart = MIMEText(text)
+msg.attach(contentPart)
+
+# Specify the path to the attachment
+etc_file_path = r'project_2023./project_progress/sample_file.txt'
+
+# reading the name of attaching file and attach it as a name of "filename"
+with open(etc_file_path, 'rb') as f :
+    etc_part = MIMEApplication( f.read() )
+    etc_part.add_header('Content-Disposition','attachment', filename = "sample_file.txt")
+    msg.attach(etc_part)
+
+# sending an email
+s = smtplib.SMTP( smtp_name, smtp_port)
+s.starttls()
+s.login( send_email , send_pwd)
+s.sendmail( send_email, recv_email, msg.as_string())
+s.quit()
