b0da8e8533c91f6e62dc36c95354df8653c2c863
KeunWoo Song <ks3651@columbia.edu>
2023-04-24 15:45:43 -0400

    added more detail examples in docs and checked Documentation Website

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,30 +9,46 @@
 import os
 import sys
 import sphinx_rtd_theme
+from recommonmark.transform import AutoStructify
 
-sys.path.insert(0, os.path.abspath(".."))
 
-project = 'EmailKeywordNotifier'
+project = 'Email Keyword Notifier'
 copyright = '2023, KeunWoo'
 author = 'KeunWoo'
-release = '0.1.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+master_doc = "index"
+extensions = [
+    "recommonmark",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo"
+    ]
+todo_include_todos = True
+autodoc_mock_imports = ["slack_sdk"]
+source_suffix = [".rst", ".md"]
 templates_path = ['_templates']
-exclude_patterns = []
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-extensions = [
-    'sphinx.ext.autodoc',
-]
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)
